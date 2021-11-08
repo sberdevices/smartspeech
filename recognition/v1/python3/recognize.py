fc7e2eaee0f493450fda48a9ee65fb692ba86e48
Semenov Andrey <Semenov.An.Vladi@sberbank.ru>
2021-11-08 11:48:00 +0300

    Revert recognize.py back to master

--- recognition/v1/python3/recognize.py
+++ recognition/v1/python3/recognize.py
@@ -87,7 +87,6 @@ class Arguments:
     DURATIONS = {'no_speech_timeout', 'max_speech_timeout', 'eou_timeout'}
     REPEATED = {'words'}
     HINTS_PREFIX = 'hints_'
-    SPEAKER_SEPARATION_PREFIX = 'speaker_separation_options_'
 
     def __init__(self):
         super().__setattr__('recognition_options', recognition_pb2.RecognitionOptions())
@@ -98,9 +97,6 @@ class Arguments:
         elif key.startswith(self.HINTS_PREFIX):
             key = key[len(self.HINTS_PREFIX):]
             self._set_option(self.recognition_options.hints, key, value)
-        elif key.startswith(self.SPEAKER_SEPARATION_PREFIX):
-            key = key[len(self.SPEAKER_SEPARATION_PREFIX):]
-            self._set_option(self.recognition_options.speaker_separation_options, key, value)
         else:
             self._set_option(self.recognition_options, key, value)
 
@@ -126,7 +122,7 @@ def create_parser():
 
     parser.add_argument('--audio-encoding', default=ENCODINGS_MAP[ENCODING_PCM], type=lambda x: ENCODINGS_MAP[x], help=str(list(ENCODINGS_MAP)))
     parser.add_argument('--sample-rate', default=16000, type=int, help='PCM only')
-    parser.add_argument('--model', default='', help=' ')
+    parser.add_argument('--model', default='general', help=' ')
     parser.add_argument('--hypotheses-count', default=1, type=int, help=' ')
     parser.add_argument('--enable-profanity-filter', action='store_true', help=' ')
     parser.add_argument('--enable-multi-utterance', action='store_true', help=' ')
@@ -137,9 +133,6 @@ def create_parser():
     parser.add_argument('--hints-enable-letters', action='store_true', help=' ')
     parser.add_argument('--hints-eou-timeout', default='0s', help=' ')
     parser.add_argument('--channels-count', default=1, type=int, help=' ')
-    parser.add_argument('--speaker-separation-options-enable', action='store_true', help=' ')
-    parser.add_argument('--speaker-separation-options-enable-only-main-speaker', action='store_true', help=' ')
-    parser.add_argument('--speaker-separation-options-count', default=0, type=int, help=' ')
 
     return parser
 
